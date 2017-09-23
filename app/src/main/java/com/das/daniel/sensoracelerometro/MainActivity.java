1bdbff6563b058febdfbbe69b6f09ec75a59d175
Daniel <daniel-sj77@hotmail.com>
2017-09-23 13:09:42 -0400

    Final

--- app/src/main/java/com/das/daniel/sensoracelerometro/MainActivity.java
+++ app/src/main/java/com/das/daniel/sensoracelerometro/MainActivity.java
@@ -1,13 +1,52 @@
 package com.das.daniel.sensoracelerometro;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    TextView _tvTexto;
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float x, y, z;
+        x=sensorEvent.values[0];
+        y=sensorEvent.values[1];
+        z=sensorEvent.values[2];
+
+        _tvTexto.setText("");
+        _tvTexto.append("Valor x: "+x+"\nValor y: "+y+"\nValor z: "+z);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        _tvTexto= (TextView) findViewById(R.id.tvTexto);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 }
